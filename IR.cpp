d359a7da0c0bb960fc4d0f5451194313a467e40d
Lionkk <Lion_yan@outlook.com>
2021-06-18 12:23:26 +0800

    更新 IR.cpp

--- IR.cpp
+++ IR.cpp
@@ -53,7 +53,7 @@ void remote_decode(void){
     uint32_t nowtime;
     while(uBit.io.P16.getDigitalValue()){//高电平等待
         nowtime = system_timer_current_time_us();
-        if((nowtime - lasttime) > 100000){//超过100 ms,表明此时没有按键按下
+        if((nowtime - lasttime) > 10000){//超过100 ms,表明此时没有按键按下
             ir_code = 0xff00;
             return;
         }
@@ -75,7 +75,7 @@ void remote_decode(void){
             nowtime = system_timer_current_time_us();
             if((nowtime - lasttime) > 500 && (nowtime - lasttime) < 700){//560us
                 //uBit.serial.printf("addr=0x%X,code = 0x%X\r\n",ir_addr,ir_code);
-                data = ir_code;
+                data = 0;
                 return;//ir_code;
             }
         }
@@ -88,44 +88,5 @@ int irCode(){
     return data;
 }
 
-//%
-int readPulseIn(int status){
-  uint32_t lasttime,nowtime,temp;
-  if(status == 1){//HIGH
-        lasttime = system_timer_current_time_us();
-	while(!uBit.io.P2.getDigitalValue()){ 
-           temp = system_timer_current_time_us();
-           if((temp - lasttime) > 70000){
-	     //uBit.serial.printf("time out 0 %d\r\n",(temp-lasttime));
-	     return -1;
-           }
-        }
-        lasttime = system_timer_current_time_us();
-	while(uBit.io.P2.getDigitalValue()){
-           if((system_timer_current_time_us() - lasttime) > 70000){
-	     //uBit.serial.printf("time out 1");
-	     return -1;
-           }
-        }
-        nowtime = system_timer_current_time_us();
-	
-  }else{//LOW
-	while(uBit.io.P2.getDigitalValue()){
-           if((system_timer_current_time_us() - lasttime) > 70000){
-	     //uBit.serial.printf("time out 3");
-	     return -1;
-           }
-        }
-	lasttime = system_timer_current_time_us();
-	while(!uBit.io.P2.getDigitalValue()){
-           if((system_timer_current_time_us() - lasttime) > 70000){
-	     //uBit.serial.printf("time out 4");
-	     return -1;
-           }
-	}
-	nowtime = system_timer_current_time_us();
-  }
-  return (nowtime - lasttime);
-}
 
 }
